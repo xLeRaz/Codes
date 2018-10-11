5a4902d8cc110b5b0adf80cd60d1fb23cc169425
xLeRaz <40789305+xLeRaz@users.noreply.github.com>
2018-10-11 14:11:02 +0200

    Update creation.js

--- creation.js
+++ creation.js
@@ -310,7 +310,27 @@ hero.on('message',async message => {
         message.channel.send(`- تم مسح **${m.size}** من الرسائل`).then(m => m.delete(5000));
       });
     }).catch();
-  }
+  } else if(args[0] === `${prefix}bc`) {
+  if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
+  if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+
+  let msgCount = 0;
+  let errorCount = 0;
+  let successCount = 0;
+  message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
+    message.guild.members.forEach(g => {
+      g.send(args.slice(1).join(' ')).then(() => {
+        successCount++;
+        msgCount++;
+        msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+      }).catch(e => {
+        errorCount++;
+        msgCount++;
+        msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+      });
+    });
+  });
+}
 });
 
 hero.login(process.env.CREATIONS);
